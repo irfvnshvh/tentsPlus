6cd332b86b056c3544dd1804e4f44f23c017887b
davier <davier570@gmail.com>
2021-01-29 16:52:50 +0800

    Update2
    Replaced css/style.css with scss
    Removed fonts folder
    Added new font, SF Pro Display
    Moved sourceimages to images
    Updated SCSS
    
    changes made to start.html
    - Fixed background image
    - Fixed size and position of logo and title
    - Updated style of login button
    - Changed sign up button
    
    changes made to login.html
    - Removed logo
    - Updated toolbar
    - Added phone number and password input fields
    - Updated login button
    
    Added 2 alert functoins
    - empty input field alert
    - wrong account details alert

--- js/script.js
+++ js/script.js
@@ -1,24 +1,30 @@
+// on document ready
 $(document).ready(function(){
     console.log("ready!");
+    // Loan in start.html on document ready
     $("ion-app").load('start.html');
 });
 
+// on click login button, load login.html
 $("body").on("click", "#login-btn", function(){
     console.log("login loaded!");
     $("ion-app").load('login.html');
 });
 
+// on click sign up text, load signup.html
 $("body").on("click", "#signup-btn", function(){
     console.log("sign up loaded!");
     $("ion-app").load('signup.html');
 });
 
-
+// on click back button, load start.html
 $("body").on("click", ".back-btn", function(){
     console.log("back to start page!");
     $("ion-app").load('start.html');
 });
 
+// remove this after finishing with login function
+// this one brings u directly into main page
 $("body").on("click", ".home-btn", function(){
     console.log("going to homepage!");
     $("ion-app").load('home.html');
@@ -28,3 +34,33 @@ $("body").on("click", "#pay-btn", function(){
     console.log("going to homepage!");
     $("ion-app").load('paymentModule.html');
 });
+
+$("body").on("click", "#testBtn", function(){
+    console.log("testBtn clicked!");
+});
+
+// Alert function for when input field is empty
+function emptyAlert() {
+    const alert = document.createElement('ion-alert');
+    alert.cssClass = 'my-custom-class';
+    alert.header = 'Error';
+    alert.message = 'Fields cannot be empty!';
+    alert.buttons = ['OK'];
+  
+    console.log("empty alert!");
+    document.body.appendChild(alert);
+    return alert.present();
+  }
+
+// Alert functoin for when wrong login details
+  function wrongDetailsAlert() {
+    const alert = document.createElement('ion-alert');
+    alert.cssClass = 'my-custom-class';
+    alert.header = 'Error';
+    alert.message = 'Wrong phone number/password';
+    alert.buttons = ['OK'];
+
+    console.log("wrong login alert!");
+    document.body.appendChild(alert);
+    return alert.present();
+  }
