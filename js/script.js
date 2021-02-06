530ba16199234ab69edbb049879c0eeb0e880f0b
davier <davier570@gmail.com>
2021-02-07 03:48:05 +0800

    Update 5
    Updated JS script
    - Added in function for stay logged in
    - Added in function for sign up
    
    Replaced sign up input fields to match test database

--- js/script.js
+++ js/script.js
@@ -1,9 +1,19 @@
 // on document ready
 $(document).ready(function () {
+
   console.log("ready!");
   // Loan in start.html on document ready
+  var userID;
+
+  //Variable to store API data
+  var dataArray;
+
   $("ion-app").load("start.html");
-});
+
+  //run stay logged in function
+  staylogged();
+
+}); // End of document ready
 
 // on click login button, load login.html
 $("body").on("click", "#gotoLogin", function () {
@@ -65,18 +75,80 @@ $("body").on("click", "#signup-btn", function () {
   console.log("signBtn clicked!");
 });
 
+$("body").on("click", "#logout-btn", function () {
+  logout();
+  console.log("logged out clicked!");
+});
+
 function gotoPage() {
   $("ion-app").load("home.html");
 }
+
+//------------------ Start of stay logged in function ----------------------------
+
+function staylogged() {
+  // if localstorage DON'T HAVE login info
+  if (localStorage.getItem("storedPhone") === null) {
+
+    console.log("no stay logged in!");
+  }
+  // if localstore HAS login info
+  else{
+    // Set localstorage value into var
+    userID = localStorage.getItem("storedID");
+    var storedPhone = localStorage.getItem("storedPhone");
+    var storedPass = localStorage.getItem("storedPass");
+
+    // settings for the API
+    var settings = {
+      url: "http://localhost:8080/api/login",
+      method: "GET",
+      timeout: 0,
+    };
+
+    //function to get data thru API
+    $.ajax(settings).done(function (response) {
+      //do this after getting data
+      //nid to parse to change from string to array
+      dataArray = JSON.parse(response);
+      console.log(dataArray);
+
+      for (var i = 0; i < dataArray.length; i++) {
+        if (storedPhone == dataArray[i].Name) {
+          if (storedPass == dataArray[i].Address) {
+            console.log("Stay logged in success!");
+            return;
+          } 
+        }
+      }
+      // end of for loop         
+    });
+  }
+  
+}
+
+//------------------ End of stay logged in function -----------------------------
+
+//-------------------- Start of log out function --------------------------------
+
+function logout(){
+  localStorage.removeItem('storedID');
+  localStorage.removeItem('storedPhone');
+  localStorage.removeItem('storedPass');
+
+  //redirect to login page here
+  $("ion-app").load("start.html");
+}
+
+//--------------------- End of log out function ---------------------------------
+
+//--------------------- Start of login function -------------------------------
 
 // Create variables to store login details
 var enteredPhone;
 var enteredPass;
 var logPhone;
 var logPass;
-
-//Variable to store API data
-var dataArray;
 
 function testLog() {
   // set the user entered value
@@ -104,81 +176,95 @@ function testLog() {
       console.log(dataArray);
       //console.log("response length is " + response.length);
       //console.log("dataArray length is " + dataArray.length);
-      
+
       //store user entered value into another var
-    // this is the var to store into database
-    logPhone = enteredPhone;
-    logPass = enteredPass;
+      // this is the var to store into database
+      logPhone = enteredPhone;
+      logPass = enteredPass;
 
-    console.log(logPass + " " + logPhone);
+      console.log(logPass + " " + logPhone);
 
-    for (var i = 0; i < dataArray.length; i++) {
-      if (enteredPhone == dataArray[i].Name) {
-        if (enteredPass == dataArray[i].Address) {
-          // Login successful!
-          // run login code here
-          console.log("Login success!");
-          return;
+      for (var i = 0; i < dataArray.length; i++) {
+        if (enteredPhone == dataArray[i].Name) {
+          if (enteredPass == dataArray[i].Address) {
+            // Login successful!
+            // run login code here
+
+            // store login info into localstorage here ---------------------------
+            userID = dataArray[i].ID;
+            //console.log("User ID is " + userID);
+
+            localStorage.setItem('storedID', userID);
+            localStorage.setItem('storedPhone', enteredPhone);
+            localStorage.setItem('storedPass', enteredPass);
+
+            console.log("Login success!");
+            return;
+          } else {
+            //invalid password
+            console.log("Wrong password!");
+            wrongDetailsAlert();
+            return;
+          }
         } else {
-          //invalid password
-          console.log("Wrong password!");
-          wrongDetailsAlert()
-          return;
-        }
-      } else {
-        //invalid phone number
-        //check to see if last entry
-        if (1 == dataArray.length - i) {
-          console.log("Phone number not found!");
-          wrongDetailsAlert()
-          return;
+          //invalid phone number
+          //check to see if last entry
+          if (1 == dataArray.length - i) {
+            console.log("Phone number not found!");
+            wrongDetailsAlert();
+            return;
+          }
         }
       }
-    }
-    // end of for loop
+      // end of for loop
     });
-
-    
   }
   // end of else if phone and password not empty
 }
 //end of test function
 
+//--------------------- End of login function -------------------------------
+
+//--------------------- Start of sign up function -------------------------------
+
 // Create variables to store signup details
-var signEnterFirst;
+/*var signEnterFirst;
 var signEnterLast;
 var signEnterPhone;
 var signEnterPass;
-var signEnterConPass;
+var signEnterConPass;*/
 
-var logSignFirst;
-var logSignLast;
-var logSignPhone;
-var logSignPass;
-var logSignConPass;
+var signName;
+var signAddress;
 
 function testSign() {
   // Assign user input values into variables
-  signEnterFirst = $("#signFirst").val();
+  /*signEnterFirst = $("#signFirst").val();
   signEnterLast = $("#signLast").val();
   signEnterPhone = $("#signPhone").val();
   signEnterPass = $("#signPass").val();
-  signEnterConPass = $("#signConPass").val();
+  signEnterConPass = $("#signConPass").val();*/
+
+  signName = $("#signName").val();
+  signAddress = $("#signAddress").val();
 
   // if user input is empty return empty alert function
   if (
-    signEnterFirst == "" ||
+    /*signEnterFirst == "" ||
     signEnterLast == "" ||
     signEnterPhone == "" ||
     signEnterPass == "" ||
-    signEnterConPass == ""
+    signEnterConPass == ""*/
+
+    signName == "" ||
+    signAddress == ""
   ) {
     console.log("is empty/null");
     emptyAlert();
   }
   // else if not empty, run store function
   else {
-    logSignFirst = signEnterFirst;
+    /*logSignFirst = signEnterFirst;
     logSignLast = signEnterLast;
     logSignPhone = signEnterPhone;
     logSignPass = signEnterPass;
@@ -193,9 +279,22 @@ function testSign() {
         logSignPass +
         " " +
         logSignConPass
-    );
-  }
+    );*/
+
+    var settings = {
+      url: `http://localhost:8080/api/signup/add?name=${signName}&address=${signAddress}`,
+      method: "POST",
+      timeout: 0,
+    };
+
+    $.ajax(settings).done(function (response) {
+      console.log("success sign");
+    });
+    // End of API function
+  } // End of else if sign in field not empty
 }
+
+//--------------------- End of login function -------------------------------
 
 // Alert function for when input field is empty
 function emptyAlert() {
@@ -222,9 +321,6 @@ function wrongDetailsAlert() {
   document.body.appendChild(alert);
   return alert.present();
 }
-
-
-
 
 //PAGE NAVIGATION LOAD JS SCRIPTS
 
